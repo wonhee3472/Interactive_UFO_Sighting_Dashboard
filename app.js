f7a941d2f53b3d6144b61246a941a5d1b2fd6313
Wonhee Yun <56327729+wonhee3472@users.noreply.github.com>
2022-01-31 13:55:34 -0600

    structuring folder

---
+++ app.js
@@ -0,0 +1,5 @@
+// import the data from data.js
+const tableData = data;
+
+// variable tbody uses d3.select to tell JavaCript to look for the <tbody> tags in the HTML page
+var tbody = d3.select("tbody");
